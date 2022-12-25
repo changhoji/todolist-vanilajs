0010c69181c5a551147169b78b02206ef1821a4d
changhoji <onkkno1@gmail.com>
2022-12-25 15:11:21 +0900

    Update: edit function

--- script.js
+++ script.js
@@ -30,27 +30,39 @@ function addTask() {
     task.className = "task";
     task.setAttribute("data-timestamp", cnt++);
 
+    //taskname
     let taskName = document.createElement("p");
     taskName.className = "task-name";
     taskName.appendChild(document.createTextNode(newTaskName));
     task.appendChild(taskName);
 
+    //check button
     let checkBtn = document.createElement("img"); //완료(체크) 버튼
     checkBtn.className = "task-check task-button unchecked";
     checkBtn.setAttribute("src", "./svg/check-square.svg");
     task.appendChild(checkBtn);
 
+    //edit button
     let editBtn = document.createElement("img");
     editBtn.className = "task-edit task-button";
     editBtn.setAttribute("src", "./svg/pencil-square.svg");
     task.appendChild(editBtn);
 
+    //edit input
+    let editInput = document.createElement("input");
+    editInput.setAttribute("type", "text");
+    editInput.className = "task-edit-input";
+    task.appendChild(editInput);
+
+    //remove button
     let removeBtn = document.createElement("img");
     removeBtn.className = "task-remove task-button";
     removeBtn.setAttribute("src", "./svg/x-square.svg");
     task.appendChild(removeBtn);
 
+    //when click "check"
     checkBtn.addEventListener("click", () => {
+        if (editInput.style.display != "none") return;
         const tasks = taskList.childNodes;
         if (checkBtn.classList.contains("unchecked")) {
             task.style.backgroundColor = "#B0B9C6";
@@ -91,6 +103,47 @@ function addTask() {
         }
     });
 
+    //when click "edit"
+    editBtn.addEventListener("click", () => {
+        if (checkBtn.classList.contains("checked")) return;
+        const beforeTaskName =
+            task.getElementsByClassName("task-name")[0].innerHTML;
+
+        //modify displays
+        taskName.style.display = "none";
+        editInput.style.display = "inline";
+
+        editInput.setAttribute("placeholder", beforeTaskName);
+
+        //add input element
+
+        //아래 두 eventlistener에 대한 함수
+        function doEditTask(e) {
+            if (editInput.style.display === "none") return;
+
+            const newTaskName = editInput.value;
+            editInput.value = "";
+            if (newTaskName === "") {
+                taskName.style.display = "";
+            }
+
+            editInput.style.display = "none";
+            taskName.style.display = "";
+            taskName.innerHTML = newTaskName;
+        }
+
+        //add input element's eventlisteners
+        editInput.addEventListener("keyup", (e) => {
+            if (e.key === "Enter") doEditTask(e);
+        });
+        editInput.addEventListener("blur", (e) => {
+            doEditTask(e);
+        });
+
+        task.prepend(editInput);
+    });
+
+    //when click "remove"
     removeBtn.addEventListener("click", () => {
         task.remove();
     });
